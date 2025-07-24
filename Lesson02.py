2819ed6168d8655d0cda6e938fc70283fd1d8668
liorse <segev.lior@gmail.com>
2025-07-24 12:42:05 +0200

    Lesson02 under fitting in the concentric circles problem

--- Lesson02.py
+++ Lesson02.py
@@ -143,3 +143,26 @@ for epoch in range(epoches):
 
 
 # %%
+# Visualize results of the training
+import requests
+from pathlib import Path
+
+# Download helper functions from pytorch
+if Path("helper_functions.py").is_file():
+    print("skipping download")
+else:
+    print("Downloading helper functions")
+    request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+    with open("helper_functions.py", 'wb') as f:
+        f.write(request.content)
+from helper_functions import plot_predictions, plot_decision_boundary 
+
+# %%
+# plot precision boundry for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1,2,1)
+plt.title("Train")
+plot_decision_boundary(model_0, X_train, y_train)
+plt.subplot(1,2,2)
+plt.title("Test")
+plot_decision_boundary(model_0, X_test, y_test)
